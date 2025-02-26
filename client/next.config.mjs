0a1743f61e1376c09f64257e355b4328cf887bcd
weiwei <weiwei@ncfc.cc>
2025-02-26 13:07:33 +0800

    change url to s3 urls

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,5 +1,14 @@
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "project-management-s3-assets.s3.us-east-1.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
